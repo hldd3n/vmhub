b5fef7905d3f77d6135f8726e59866801e659d49
Nikolay Vassilev <nikolay.vassilev@synchronoss.com>
2020-07-06 17:44:00 +0300

    feat: 🎸 added filtering and sorting to repository list datagrid

--- src/app/components/repository/repository-list/repository-list.component.ts
+++ src/app/components/repository/repository-list/repository-list.component.ts
@@ -1,25 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { map, takeUntil } from 'rxjs/operators';
+import { RepositoryService } from '../../../services/repository/repository.service';
+import { SubscribedComponent } from '../../shared/subscribed.component';
+import { ClrDatagridSortOrder } from '@clr/angular';
 
 @Component({
     selector: 'app-repository-list',
     templateUrl: './repository-list.component.html',
     styleUrls: ['./repository-list.component.scss']
 })
-export class RepositoryListComponent implements OnInit {
+export class RepositoryListComponent extends SubscribedComponent implements OnInit {
 
     public repositories: any[] = []
+    public sort = ClrDatagridSortOrder.DESC;
 
-    constructor() { }
+    constructor(
+        private readonly router: Router,
+        private readonly repositoryService: RepositoryService,
+        private _activatedRoute: ActivatedRoute,
+    ) {
+        super()
+     }
 
     ngOnInit(): void {
-        this.repositories = [{
-            name: 'FirstRepo',
-            license: 'none',
-            commits: 15,
-            contributors: ['ivan', 'dragan'],
-            releases: 'dobre',
-            branches: ['master', 'develop'],
-        }]
+        this.repositoryService
+            .getRepositories()
+            .pipe(takeUntil(
+                this.componentDestroyed$
+            ))
+            .subscribe((repositories) => {
+                this.repositories = [...repositories];
+            });
     }
 
+    public handleRowClick(repository): void {
+        console.log(repository.name);
+        this.router.navigate(['repositories', `${repository.name}`])
+    }
 }
