0ecc9497ff3b3f22c63639d6ec8a9a5e9bd8538c
hldd3n <nikolaivassilev@gmail.com>
2020-08-05 12:24:26 +0300

    fix: 🐛 fixed sorting the datagrid

--- src/app/components/repository/repository-list/repository-list.component.ts
+++ src/app/components/repository/repository-list/repository-list.component.ts
@@ -2,9 +2,16 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { RepositoryService, } from '../../../services/repository/repository.service';
 import { SubscribedComponent } from '../../shared/subscribed.component';
-import { ClrDatagridSortOrder, ClrDatagridStateInterface } from '@clr/angular';
-import { IRepository } from '../../../common/interfaces/repository.interface';
+import { ClrDatagridSortOrder, ClrDatagridStateInterface, ClrDatagridComparatorInterface } from '@clr/angular';
+import { IRepository, IRepositoryLicense, IContributor } from '../../../common/interfaces/repository.interface';
 import { tap } from 'rxjs/operators';
+
+class ContributorComparator implements ClrDatagridComparatorInterface<IContributor> {
+    compare(a: IContributor, b: IContributor) {
+        console.log('CONTRIBB', a, b)
+        return a.commits - b.commits;
+    }
+}
 
 @Component({
     selector: 'app-repository-list',
@@ -16,11 +23,11 @@ export class RepositoryListComponent extends SubscribedComponent implements Afte
     public repositories: IRepository[] = []
     public sort = ClrDatagridSortOrder.DESC;
     public loading = true;
+    public contributorComparator = new ContributorComparator();
 
     constructor(
         private readonly router: Router,
         private readonly repositoryService: RepositoryService,
-        private _activatedRoute: ActivatedRoute,
     ) {
         super()
     }
